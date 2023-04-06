13afa70994e1d42cb20425b87907d4386a9fc1b1
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-04-06 18:59:43 +0800

    Add files via upload

---
+++ SpaService.java
@@ -0,0 +1,24 @@
+//Santiago, Bryan Aaron B. | ICT-101
+
+public class SpaService 
+{
+    private String serviceDescription;
+    private double price;
+
+    public void setServiceDescription(String service)
+    {
+        serviceDescription = service;
+    }
+    public void setPrice(double pr)
+    {
+        price = pr;
+    }
+    public String getServiceDescription()
+    {
+        return serviceDescription;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+}
