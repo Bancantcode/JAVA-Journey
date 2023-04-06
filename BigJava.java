083bae6531410c00bf1bb821f678910fbadd7549
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-04-06 18:57:59 +0800

    Add files via upload

---
+++ BigJava.java
@@ -0,0 +1,19 @@
+public class BigJava
+{
+	public static void main(String[] args)
+	{
+		System.out.println();
+		System.out.println("           JJJJJJJJJJJJJ      AAAAA      VV           VV      AAAAA");
+		System.out.println("                  JJ         AA   AA     VV           VV     AA   AA");
+		System.out.println("                  JJ        AA     AA    VV           VV    AA     AA");
+		System.out.println("                  JJ       AA       AA    VV         VV    AA       AA");
+		System.out.println("                  JJ       AAAAAAAAAAA     VV       VV     AAAAAAAAAAA");
+		System.out.println("           JJ     JJ       AAAAAAAAAAA      VV     VV      AAAAAAAAAAA");
+		System.out.println("            JJ    JJ       AA       AA       VV   VV       AA       AA");
+		System.out.println("            JJJ  JJ        AA       AA        VV VV        AA       AA");
+		System.out.println("             JJJJJ         AA       AA         VVV         AA       AA");
+		System.out.println();
+	}
+}
+
+//Santiago, Bryan Aaron B.c
