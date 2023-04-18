60ee73390709ee75f73785dd0d29e214ae45278b
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-04-18 22:53:42 +0800

    Add files via upload

---
+++ ArrayIndexOfFirstOccurence.java
@@ -0,0 +1,14 @@
+public class ArrayIndexOfFirstOccurence 
+{
+    public static int findIndex(int[] arr, int target) 
+    {
+        for (int i = 0; i < arr.length; i++) 
+        {
+            if (arr[i] == target) 
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
