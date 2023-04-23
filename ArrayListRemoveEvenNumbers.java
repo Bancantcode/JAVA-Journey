30c70ff039c524b75ec0bc997b6be354f841f82a
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-04-23 22:48:34 +0800

    Add files via upload

---
+++ ArrayListRemoveEvenNumbers.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class ArrayListRemoveEvenNumbers 
+{
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(10);
+
+        for (int i = 0; i < numbers.size(); i++) 
+        {
+            if (numbers.get(i) % 2 == 0) 
+            {
+                numbers.remove(i);
+                i--;
+            }
+        }
+
+        for (int number : numbers) 
+        {
+            System.out.println(number);
+        }
+    }
+}
