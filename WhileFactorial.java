42de394979c73bf0bb4b0864c0c3d1af580f1940
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-04-29 22:01:40 +0800

    Add files via upload

---
+++ WhileFactorial.java
@@ -0,0 +1,15 @@
+public class WhileFactorial 
+{
+    public static void main(String[] args) 
+    {
+        int n = 5;
+        int factorial = 1;
+        int i = 1;
+        while(i <= n)
+        {
+            factorial *= i;
+            i++;
+        }
+        System.out.println("Factorial of "+n+" is: "+factorial);
+    }    
+}
