9fbfbd9d660d014a9eaf2010630863dda19cb6ee
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-04-29 22:02:07 +0800

    Add files via upload

---
+++ WhilePrimeOrNot.java
@@ -0,0 +1,25 @@
+public class WhilePrimeOrNot 
+{
+    public static void main(String[] args) 
+    {
+        int num = 7;
+        int i = 2;
+        boolean isPrime = true;
+        while(i <= num/2){
+            if(num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+            i++;
+        }
+        if(isPrime)
+        {
+            System.out.println(num+" is a prime number.");
+        } 
+        else 
+        {
+            System.out.println(num+" is not a prime number.");
+        }        
+    }    
+}
