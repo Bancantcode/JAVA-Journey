4089a6d420ca5d39f44da839097fd97db01ab4a9
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-05-01 20:26:42 +0800

    Add files via upload

---
+++ BooleanEndsWith.java
@@ -0,0 +1,6 @@
+///Write a method that takes in two strings and returns true if the first string ends with the second string, and false otherwise.
+
+public static boolean endsWith(String str, String suffix) 
+{
+    return str.endsWith(suffix);
+}
