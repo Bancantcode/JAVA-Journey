20e5e6ea340e87dc68df40c45d18470c48fd85d8
Bryan Aaron Santiago <126086817+Bancantcode@users.noreply.github.com>
2023-10-14 02:47:14 +0800

    Add files via upload

---
+++ DataMessage.java
@@ -0,0 +1,13 @@
+public class DataMessage 
+{
+    public static String[] message = 
+    {
+        "Valid Input.", //0
+        "Data not valid choice", //1
+        "Rental Days is too low.", //2
+        "Rental Days is too high", //3
+        "Zip Code is too low", //4
+        "Zip Code is too high", //5
+        "Data is not numeric" //6 - inputMismatch
+    };    
+}
